0a5841364f876133b0033819e86e4809625e423e
pgrole <pgrolemodelgmail.com>
2018-10-18 11:16:23 +0900

    Initial Commit

---
+++ imageTransfer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  imageTransfer
+//
+//  Created by ding qi on 2018/10/18.
+//  Copyright © 2018 ding qi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
